fc8d9d48d55bba6339f90fba5bf90b3cf5729bff
Satkuri Kailash <84554842+KailashSatkuri-warangal@users.noreply.github.com>
2025-09-18 16:36:47 +0530

    workflow

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,4 +1,8 @@
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'text/markdown');
-  res.send(`# Hello from Vercel!\n\nThis is a placeholder README.`);
-}
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/markdown'});
+  res.end(`# KailashSatkuri-warangal\n\nHello! This is your README content.`);
+});
+
+server.listen(process.env.PORT || 3000);
