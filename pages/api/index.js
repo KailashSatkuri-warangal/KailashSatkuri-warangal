3a8617218fccd2bd4226ef60e49b9628d03b2096
Satkuri Kailash <84554842+KailashSatkuri-warangal@users.noreply.github.com>
2025-09-18 16:39:02 +0530

    workflow

--- pages/api/index.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/markdown'});
-  res.end(`# KailashSatkuri-warangal\n\nHello! This is your README content.`);
-});
-
-server.listen(process.env.PORT || 3000);
